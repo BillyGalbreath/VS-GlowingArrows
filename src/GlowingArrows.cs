6ffa9830c3357f0ed70edd7423f69c50e2a5d738
William Blake Galbreath <Blake.Galbreath@GMail.com>
2023-09-01 17:57:22 -0500

    glow all projectiles

--- src/GlowingArrows.cs
+++ src/GlowingArrows.cs
@@ -1,24 +1,29 @@
-﻿using System.Collections.Generic;
-using System.Linq;
-using Vintagestory.API.Client;
+﻿using Vintagestory.API.Client;
 using Vintagestory.API.Common;
 using Vintagestory.API.Common.Entities;
 
 namespace GlowingArrows;
 
 public class GlowingArrows : ModSystem {
-    private static readonly List<string> PROJECTILE = new() { "arrow", "spear" };
+    private ICoreClientAPI api;
 
     public override bool ShouldLoad(EnumAppSide side) {
         return side.IsClient();
     }
 
-    public override void StartClientSide(ICoreClientAPI api) {
-        api.Event.RegisterCallback(_ => {
-            foreach (EntityProperties entityProperties in api.World.EntityTypes.Where(entityProperties => PROJECTILE.Any(entityProperties.Code.ToString()!.Contains))) {
-                entityProperties.Client.GlowLevel = 255;
-                Mod.Logger.Event($"Set {entityProperties.Code} to glow!");
-            }
-        }, 1000);
+    public override void StartClientSide(ICoreClientAPI capi) {
+        api = capi;
+
+        api.Event.OnEntitySpawn += GlowThoseArrows;
+    }
+
+    public override void Dispose() {
+        api.Event.OnEntitySpawn -= GlowThoseArrows;
+    }
+
+    private static void GlowThoseArrows(Entity entity) {
+        if (entity.GetType().ToString().ToLower().Contains("projectile")) {
+            entity.Properties.Client.GlowLevel = 255;
+        }
     }
 }
