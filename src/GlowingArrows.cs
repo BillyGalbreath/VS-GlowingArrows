a2ce8fe6ee8890be6312e234d4cd5660b12119ed
William Blake Galbreath <Blake.Galbreath@GMail.com>
2023-09-11 11:31:03 -0500

    update glowing property on startup instead of when the entity spawns in. fixes issue where unloaded projectiles lose their glow when loaded back.

--- src/GlowingArrows.cs
+++ src/GlowingArrows.cs
@@ -1,59 +1,27 @@
-﻿using Vintagestory.API.Client;
+﻿using System.Linq;
+using Vintagestory.API.Client;
 using Vintagestory.API.Common;
 using Vintagestory.API.Common.Entities;
+using Vintagestory.Common;
 using Vintagestory.GameContent;
 
 namespace GlowingArrows;
 
 public class GlowingArrows : ModSystem {
-    // internal reference to the client's api
-    private ICoreClientAPI api;
-
     public override bool ShouldLoad(EnumAppSide side) {
-        // only load if on the client side
         return side.IsClient();
     }
 
-    public override void StartClientSide(ICoreClientAPI capi) {
-        // store reference to the api (for later use)
-        api = capi;
-
-        // add our logic to entity spawn event
-        api.Event.OnEntitySpawn += MakeProjectilesGlow;
-    }
-
-    public override void Dispose() {
-        // make sure api is not null (in case the mod never fully started)
-        if (api != null) {
-            // remove our logic from entity spawn event
-            api.Event.OnEntitySpawn -= MakeProjectilesGlow;
-        }
-    }
-
-    /// <summary>
-    /// If the passed entity is a projectile, make it glow at full brightness (0xFF)
-    /// </summary>
-    /// <param name="entity"></param>
-    private static void MakeProjectilesGlow(Entity entity) {
-        // verify the entity is a projectile
-        if (IsProjectile(entity)) {
-            // make the projectile glow
-            entity.Properties.Client.GlowLevel = 0xFF;
-        }
-    }
-
-    /// <summary>
-    /// Check if the passed object is a projectile.
-    /// </summary>
-    /// <param name="obj"></param>
-    /// <returns>Returns true if one of the following are true:
-    /// <li>a standard <see cref="Vintagestory.GameContent.EntityProjectile">EntityProjectile</see></li>
-    /// <li>registered class name contains the word "projectile" (case insensitive)</li>
-    /// <li>object's type name contains the word "projectile" (case insensitive)</li>
-    /// </returns>
-    private static bool IsProjectile(RegistryObject obj) {
-        return obj is EntityProjectile ||
-               obj.Class.ToLower().Contains("projectile") ||
-               obj.GetType().ToString().ToLower().Contains("projectile");
+    public override void StartClientSide(ICoreClientAPI api) {
+        api.Event.RegisterCallback(_ => {
+            foreach (EntityProperties properties in from properties in api.World.EntityTypes
+                     let mapping = ((ClassRegistryAPI)api.ClassRegistry).GetField<ClassRegistry>("registry").entityClassNameToTypeMapping
+                     where mapping[properties.Class].IsAssignableFrom(typeof(EntityProjectile))
+                           || properties.Class.ToLower().Contains("projectile")
+                           || (properties.Code.ToString()?.ToLower().Contains("projectile") ?? false)
+                     select properties) {
+                properties.Client.GlowLevel = 0xFF;
+            }
+        }, 1000);
     }
 }
---
+++ src/ReflectionHelper.cs
@@ -0,0 +1,9 @@
+﻿using System.Reflection;
+
+namespace GlowingArrows;
+
+public static class ReflectionHelper {
+    public static T GetField<T>(this object obj, string name) {
+        return (T)obj.GetType().GetField(name, BindingFlags.NonPublic | BindingFlags.Instance)?.GetValue(obj);
+    }
+}
