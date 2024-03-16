a757d68b7a6010731e55214c65e58acb4df33b3b
William Blake Galbreath <Blake.Galbreath@GMail.com>
2024-03-15 21:34:01 -0500

    fix possible null references

--- src/GlowingArrows.cs
+++ src/GlowingArrows.cs
@@ -1,4 +1,6 @@
-﻿using System.Linq;
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
 using Vintagestory.API.Client;
 using Vintagestory.API.Common;
 using Vintagestory.API.Common.Entities;
@@ -14,13 +16,13 @@ public class GlowingArrows : ModSystem {
 
     public override void StartClientSide(ICoreClientAPI api) {
         api.Event.RegisterCallback(_ => {
-            foreach (EntityProperties properties in from properties in api.World.EntityTypes
-                     let mapping = ((ClassRegistryAPI)api.ClassRegistry).GetField<ClassRegistry>("registry").entityClassNameToTypeMapping
-                     where mapping[properties.Class].IsAssignableFrom(typeof(EntityProjectile))
-                           || properties.Class.ToLower().Contains("projectile")
-                           || (properties.Code.ToString()?.ToLower().Contains("projectile") ?? false)
-                     select properties) {
-                properties.Client.GlowLevel = 0xFF;
+            Dictionary<string, Type>? mapping = ((ClassRegistryAPI)api.ClassRegistry).GetField<ClassRegistry>("registry")?.entityClassNameToTypeMapping;
+            foreach (EntityProperties entityProperties in api.World.EntityTypes.Where(properties =>
+                         (mapping != null && mapping[properties.Class].IsAssignableFrom(typeof(EntityProjectile)))
+                         || properties.Class.ToLower().Contains("projectile")
+                         || (properties.Code.ToString()?.ToLower().Contains("projectile") ?? false)
+                     )) {
+                entityProperties.Client.GlowLevel = 0xFF;
             }
         }, 1000);
     }
--- src/ReflectionHelper.cs
+++ src/ReflectionHelper.cs
@@ -3,7 +3,7 @@
 namespace GlowingArrows;
 
 public static class ReflectionHelper {
-    public static T GetField<T>(this object obj, string name) {
-        return (T)obj.GetType().GetField(name, BindingFlags.NonPublic | BindingFlags.Instance)?.GetValue(obj);
+    public static T? GetField<T>(this object obj, string name) where T : class {
+        return obj.GetType().GetField(name, BindingFlags.NonPublic | BindingFlags.Instance)?.GetValue(obj) as T;
     }
 }
